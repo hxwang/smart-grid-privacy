da18013a5544f20ac265b1100ceeb89c9c81b680
Huangxin Wang <maggiewangsjtu@gmail.com>
2015-04-14 01:40:11 -0400

    add implement of ls2

--- code/smart-grid-privacy/Algorithm/LS2.cs
+++ code/smart-grid-privacy/Algorithm/LS2.cs
@@ -7,14 +7,31 @@ namespace smart_grid_privacy.Algorithm
 {
     public class LS2: AlgBase
     {
+        public double BatteryParameter;
+        public int Multiple;
+        public double lowEnergyLine;
+        public double highEnergyLine;
+        public Boolean IsCharging;
+        public enum State { Low, Mid, High}
+
 
         public LS2() {
             this.AlgType = AlgType.LS2;
         }
 
+        public void init() {
+            this.BatteryParameter = Math.Min(this.Battery.MaximumChargeRate, -this.Battery.MaximumDischargeRate);
+        }
         public override void DecideEnergy(int time)
         {
-            throw new NotImplementedException();
+            double batteryPower = 0;
+            if (time == 0)
+            {
+                batteryPower = 0;
+            }
+            else { 
+                
+            }
         }
     }
 }
